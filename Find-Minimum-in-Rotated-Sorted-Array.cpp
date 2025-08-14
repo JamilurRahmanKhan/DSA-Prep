c03c00eb82e2774fe283b45171baefd2e049cd69
JamilurRahmanKhan <jamilurrahman1152@gmail.com>
2025-08-14 22:55:43 +0600

    Add: Find-Minimum-in-Rotated-Sorted-Array (NeetCode #17)

---
+++ Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    int findMin(vector<int> &nums)
+    {
+        int l = 0, r = nums.size() - 1;
+
+        while (l < r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {3, 4, 5, 6, 1, 2};
+    cout << sol.findMin(nums) << endl;
+    return 0;
+}
