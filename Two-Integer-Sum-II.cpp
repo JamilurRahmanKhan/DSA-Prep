708fb1ac9380e7c9e2d748eca399b45b903214d0
Jamilur Rahman Khan <jamilurrahman1152@gmail.com>
2026-03-29 11:46:48 +0600

    Add Two-Integer-Sum-II solution

--- Two-Integer-Sum-II.cpp
+++ Two-Integer-Sum-II.cpp
@@ -5,18 +5,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0, r = numbers.size() - 1;
+        int l = 0;
+        int r = numbers.size() - 1;
 
-        while (l < r) {
-            int sum = numbers[l] + numbers[r];
-            if (sum > target) {
-                r--;
-            }
-            else if (sum < target) {
+        while(l<r){
+            int sum = numbers[l]+numbers[r];
+            if(sum<target){
                 l++;
-            }
-            else {
-                return {l + 1, r + 1}; 
+            }else if(sum>target){
+                r--;
+            }else{
+                return {l+1,r+1};
             }
         }
         return {};
